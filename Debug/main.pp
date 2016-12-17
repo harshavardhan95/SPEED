76b38a3cc98b4829c36c60ebf2ee45f464096af7
harshavardhan95 <harshavardhan9510@gmail.com>
2016-12-17 18:51:19 +0530

    Second commit

---
+++ Debug/main.pp
@@ -0,0 +1,91 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_i2c.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/rom.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/i2c.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/tm4c123gh6pge.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/ssi.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/interrupt.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/uart.h
+main.obj: ../utils/uartstdio.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_nvic.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/timer.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/udma.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/flash.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_ssi.h
+main.obj: ../ISR.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/adc.h
+main.obj: ../I2C.h
+main.obj: ../ISR.h
+main.obj: ../Functions.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/systick.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pwm.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/watchdog.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_watchdog.h
+main.obj: ../I2C.h
+main.obj: ../Functions.h
+main.obj: ../HV_Control.h
+main.obj: ../SoftLED.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_ints.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/adc.c
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_adc.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/debug.h
+
+../main.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_i2c.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/i2c.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/tm4c123gh6pge.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/ssi.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/uart.h: 
+../utils/uartstdio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_nvic.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/udma.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/flash.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_ssi.h: 
+../ISR.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/adc.h: 
+../I2C.h: 
+../ISR.h: 
+../Functions.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pwm.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/watchdog.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_watchdog.h: 
+../I2C.h: 
+../Functions.h: 
+../HV_Control.h: 
+../SoftLED.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/adc.c: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_adc.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/debug.h: 
